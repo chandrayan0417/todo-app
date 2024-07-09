b69318d43a4ded3df1c39074f081255ef29cdadb
Chandrayan Roy <chandrayanroy@gmail.com>
2024-07-10 00:03:49 +0530

    db completed
    db completed

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,17 @@
 const mongoose = require("mongoose");
+
 mongoose.connect(
   "mongodb+srv://chandrayan0417:a0pvr3AXY8FUnWGK@chansrayan0417mdb.adbvx54.mongodb.net/todo-app"
 );
+
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  completed: { type: Boolean, default: false },
+});
+
+const Todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  Todo,
+};
--- backend/main.js
+++ backend/main.js
@@ -1,19 +1,62 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
+const { Todo } = require("./db");
+const cors = require("cors");
 const app = express();
 
-
 app.use(express.json());
+app.use(cors());
 
-app.get("/todos", (res, req) => {
+app.post("/todo", async function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
 
-});
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    // put it in mongodb
+    await Todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
 
-app.post("/cerate-todo", (res, req) => {
+    res.json({
+        msg: "Todo created"
+    })
+})
 
-});
+app.get("/todos", async function(req, res) {
+  const todos = await Todo.find({});
 
-app.put("/completed", (res, req)=>{
+    res.json({
+        todos
+    })
 
-});
+})
+
+app.put("/completed", async function(req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+
+    await Todo.updateOne({
+        _id: req.body.id
+    }, {
+      completed: true  
+    })
+
+    res.json({
+        msg: "Todo marked as completed"
+    })
+})
 
 app.listen(3000);
